744c5742dbe20eb607d993bee080e2bb6ca1cf88
stepin104353 <69048783+stepin104353@users.noreply.github.com>
2020-09-19 09:36:15 +0530

    Update assignment.c

--- assignment.c
+++ assignment.c
@@ -1,146 +1,218 @@
-#include <stdio.h>
-#include <conio.h>
+// Calculator example using C code
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
 
-char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+#define KEY "Enter the calculator Operation you want to do:"
 
-int checkwin();
-void board();
+// Function prototype declaration
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+void squareroot();
+int factorial();
+void calculator_operations();
 
+// Start of Main Program
 int main()
 {
-    int player = 1, i, choice;
+    int X=1;
+    char Calc_oprn;
 
-    char mark;
-    do
+    // Function call 
+    calculator_operations();
+
+    while(X)
     {
-        board();
-        player = (player % 2) ? 1 : 2;
+        printf("\n");
+        printf("%s : ", KEY);
 
-        printf("Player %d, enter a number:  ", player);
-        scanf("%d", &choice);
+        Calc_oprn=getch();
 
-        mark = (player == 1) ? 'X' : 'O';
-
-        if (choice == 1 && square[1] == '1')
-            square[1] = mark;
-            
-        else if (choice == 2 && square[2] == '2')
-            square[2] = mark;
-            
-        else if (choice == 3 && square[3] == '3')
-            square[3] = mark;
-            
-        else if (choice == 4 && square[4] == '4')
-            square[4] = mark;
-            
-        else if (choice == 5 && square[5] == '5')
-            square[5] = mark;
-            
-        else if (choice == 6 && square[6] == '6')
-            square[6] = mark;
-            
-        else if (choice == 7 && square[7] == '7')
-            square[7] = mark;
-            
-        else if (choice == 8 && square[8] == '8')
-            square[8] = mark;
-            
-        else if (choice == 9 && square[9] == '9')
-            square[9] = mark;
-            
-        else
+        switch(Calc_oprn)
         {
-            printf("Invalid move ");
+            case '+': addition();
+                      break;
 
-            player--;
-            getch();
+            case '-': subtraction();
+                      break;
+
+            case '*': multiplication();
+                      break;
+
+            case '/': division();
+                      break;
+
+            case '?': modulus();
+                      break;
+
+            case '!': factorial();
+                      break;
+                      
+            case '$': squareroot();
+                      break;
+
+            case '^': power();
+                      break;
+
+            case 'H':
+            case 'h': calculator_operations();
+                      break;
+
+            case 'Q':
+            case 'q': exit(0);
+                      break;
+            case 'c':
+            case 'C': system("cls");
+                      calculator_operations();
+                      break;
+
+            default : system("cls");
+
+    printf("\n**********You have entered unavailable option");
+    printf("***********\n");
+    printf("\n*****Please Enter any one of below available ");
+    printf("options****\n");
+                      calculator_operations();
         }
-        i = checkwin();
+    }
+}
 
-        player++;
-    }while (i ==  - 1);
-    
-    board();
-    
-    if (i == 1)
-        printf("==>\aPlayer %d win ", --player);
-    else
-        printf("==>\aGame draw");
+//Function Definitions
 
-    getch();
+void calculator_operations()
+{
+    //system("cls");  use system function to clear 
+    //screen instead of clrscr();
+    printf("\n             Welcome to C calculator \n\n");
 
+    printf("******* Press 'Q' or 'q' to quit ");
+    printf("the program ********\n");
+    printf("***** Press 'H' or 'h' to display ");
+    printf("below options *****\n\n");
+    printf("Enter 'C' or 'c' to clear the screen and");
+    printf(" display available option \n\n");
+
+    printf("Enter + symbol for Addition \n");
+    printf("Enter - symbol for Subtraction \n");
+    printf("Enter * symbol for Multiplication \n");
+    printf("Enter / symbol for Division \n");
+    printf("Enter ? symbol for Modulus\n");
+    printf("Enter ^ symbol for Power \n");
+    printf("Enter ! symbol for Factorial \n\n");
+}
+
+void addition()
+{
+    int n, total=0, k=0, number;
+    printf("\nEnter the number of elements you want to add:");
+    scanf("%d",&n);
+    printf("Please enter %d numbers one by one: \n",n);
+    while(k<n)
+    { 
+        scanf("%d",&number);
+        total=total+number;
+        k=k+1;
+    }
+    printf("Sum of %d numbers = %d \n",n,total);
+}
+
+void subtraction()
+{ 
+    int a, b, c = 0; 
+    printf("\nPlease enter first number  : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number : "); 
+    scanf("%d", &b); 
+    c = a - b; 
+    printf("\n%d - %d = %d\n", a, b, c); 
+}
+
+void multiplication()
+{
+    int a, b, mul=0; 
+    printf("\nPlease enter first numb   : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number: "); 
+    scanf("%d", &b);
+    mul=a*b;
+    printf("\nMultiplication of entered numbers = %d\n",mul);
+}
+
+void division()
+{
+    int a, b, d=0; 
+    printf("\nPlease enter first number  : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number : "); 
+    scanf("%d", &b);
+    d=a/b;
+    printf("\nDivision of entered numbers=%d\n",d);
+}
+
+void modulus()
+{
+    int a, b, d=0; 
+    printf("\nPlease enter first number   : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number  : "); 
+    scanf("%d", &b);
+    d=a%b;
+    printf("\nModulus of entered numbers = %d\n",d);
+}
+
+void squareroot()
+{
+    int a,s;
+    printf("Enter a number to find the square root");
+    scanf("%d",&a);
+    s=sqrt(a);
+    printf("\nSquare root of the number is %d",s);
+}
+
+
+void power()
+{
+    double a,num, p;
+    printf("\nEnter two numbers to find the power \n");
+    printf("number: ");
+    scanf("%lf",&a);
+
+    printf("power : ");
+    scanf("%lf",&num);
+
+    p=pow(a,num);
+
+    printf("\n%lf to the power %lf = %lf \n",a,num,p);
+}
+
+int factorial()
+{
+    int i,fact=1,num;
+
+    printf("\nEnter a number to find factorial : ");
+    scanf("%d",&num);
+
+    if (num<0)
+    {
+        printf("\nPlease enter a positive number to");
+        printf(" find factorial and try again. \n");
+        printf("\nFactorial can't be found for negative");
+        printf(" values. It can be only positive or 0  \n");
+        return 1;
+    }               
+
+    for(i=1;i<=num;i++)
+   {
+       fact=fact*i;
+    printf("\n");
+    printf("Factorial of entered number %d is:%d\n",num,fact);
     return 0;
 }
 
-/*********************************************
-
-FUNCTION TO RETURN GAME STATUS
-1 FOR GAME IS OVER WITH RESULT
--1 FOR GAME IS IN PROGRESS
-O GAME IS OVER AND NO RESULT
- **********************************************/
-
-int checkwin()
-{
-    if (square[1] == square[2] && square[2] == square[3])
-        return 1;
-        
-    else if (square[4] == square[5] && square[5] == square[6])
-        return 1;
-        
-    else if (square[7] == square[8] && square[8] == square[9])
-        return 1;
-        
-    else if (square[1] == square[4] && square[4] == square[7])
-        return 1;
-        
-    else if (square[2] == square[5] && square[5] == square[8])
-        return 1;
-        
-    else if (square[3] == square[6] && square[6] == square[9])
-        return 1;
-        
-    else if (square[1] == square[5] && square[5] == square[9])
-        return 1;
-        
-    else if (square[3] == square[5] && square[5] == square[7])
-        return 1;
-        
-    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
-        square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] 
-        != '7' && square[8] != '8' && square[9] != '9')
-
-        return 0;
-    else
-        return  - 1;
 }
 
-
-/*******************************************************************
-FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
- ********************************************************************/
-
-
-void board()
-{
-    system("cls");
-    printf("\n\n\tTic Tac Toe\n\n");
-
-    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
-
-
-    printf("     |     |     \n");
-    printf("  %c  |  %c  |  %c \n", square[1], square[2], square[3]);
-
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-
-    printf("  %c  |  %c  |  %c \n", square[4], square[5], square[6]);
-
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-
-    printf("  %c  |  %c  |  %c \n", square[7], square[8], square[9]);
-
-    printf("     |     |     \n\n");
-}
