8aaab2c1450b6afcd0b31a46372df01afd3ee400
stepin104353 <69048783+stepin104353@users.noreply.github.com>
2020-09-19 11:10:32 +0530

    header file

---
+++ inc/header.c
@@ -0,0 +1,18 @@
+// Calculator example using C code
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+
+#define KEY "Enter the calculator Operation you want to do:"
+
+// Function prototype declaration
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+void squareroot();
+int factorial();
+void calculator_operations();
